41a8fa45c1f6ab6a3ac41ea83d78a9ff0fe1a6ec
nazializu <tarafdartarafdarlizu@gmail.com>
2024-07-16 22:27:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var dimErPacket = [12, 14, 15, 16,20,20,14];
+var studentName = ["tania" , "shahera" , "lizu" , "doly", "tanvir" , "hakim" ,"faiz"]
+var isPresent= [true , true,true ,true, false,false,false];
+console.log(studentName[3])
+console.log(isPresent[5])
